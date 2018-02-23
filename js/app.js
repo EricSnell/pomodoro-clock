c2c40604614dbe87c49fdc4e7817e46f117c5473
eric <ericmichaelsnell@gmail.com>
2018-02-22 20:46:42 -0500

    click events for timer length controls -- updates state

--- js/app.js
+++ js/app.js
@@ -1,7 +1,47 @@
 (() => {
   'use strict'
 
+  const [settings] = Array.from(document.getElementsByClassName('settings'));
+  const breakDisplay = document.getElementById('break-display');
+  const sessionDisplay = document.getElementById('session-display');
+
+  let state = {
+    breakLength: 5,
+    sessionLength: 25
+  }
+
+  settings.addEventListener('click', (e) => {
+    const target = e.target.id;
+    let newState;
+    switch (target) {
+      case 'break-increase':
+        newState = state.breakLength + 1;
+        updateState({ breakLength: newState });
+        break;
+      case 'break-decrease':
+        newState = state.breakLength > 0 ? state.breakLength - 1 : 0;
+        updateState({ breakLength: newState });
+        break;
+      case 'session-increase':
+        newState = state.sessionLength + 1;
+        updateState({ sessionLength: newState });
+        break;
+      case 'session-decrease':
+        newState = state.sessionLength > 0 ? state.sessionLength - 1 : 0;
+        updateState({ sessionLength: newState })
+        break;
+      default:
+        break;
+    }
+  })
 
 
+
+
+
+  function updateState(newState = {}) {
+    state = Object.assign({}, state, newState);
+    console.log(state);
+  }
 
 })()
