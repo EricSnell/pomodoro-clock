1fe6ac433900530810964cb861ab96f0b2410cc1
eric <ericmichaelsnell@gmail.com>
2018-03-04 17:07:05 -0500

    comments added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 (() => {
   'use strict'
 
+  // Caching timer elements
   const [settings] = Array.from(document.getElementsByClassName('settings'));
   const [timerDisplay] = Array.from(document.getElementsByClassName('timer'));
+  const [timerBtn] = Array.from(document.getElementsByClassName('timer-btn'));
   const breakDisplay = document.getElementById('break-display');
   const sessionDisplay = document.getElementById('session-display');
-  const [timerBtn] = Array.from(document.getElementsByClassName('timer-btn'));
   const progressBar = document.getElementById('progress');
 
+  // Variable to store setTimeout method (allows us to clear method later)
   let timer;
 
+  // States of timer
   let state = {
     breakLength: 5,
     sessionLength: 25,
@@ -20,14 +23,14 @@
 
   init();
 
-
-
+  // Adds event listeners and updates display with initial state
   function init() {
     settings.addEventListener('click', adjustTimer);
     timerBtn.addEventListener('click', handleButton);
     updateDisplay();
   }
 
+  // Handles break and session timer adjustments (only when timer not running)
   function adjustTimer(e) {
     const target = e.target.id;
     if (state.paused) {
@@ -56,6 +59,7 @@
     }
   }
 
+  // Pauses/Unpauses timer
   function handleButton(e) {
     e.stopPropagation();
     togglePaused();
@@ -69,11 +73,14 @@
     }
   }
 
+  // Toggles paused state
   function togglePaused() {
     const toggle = !state.paused;
     updateState({ paused: toggle });
   }
 
+  // Executes the timer countdown, assigning a setInterval to "timer" variable
+  // toggling between session and break
   function runTimer() {
     timer = setInterval(() => {
       if (state.currentCount > 0) {
@@ -86,12 +93,18 @@
     }, 1000);
   }
 
+  // Clears the setInterval method that runs the timer
+  function stopTimer() {
+    clearInterval(timer);
+  }
+
   function playSound() {
     const mp3 = 'http://newt.phys.unsw.edu.au/music/bellplates/sounds/bellplate-corner3.mp3';
     const sound = new Audio(mp3);
     sound.play();
   }
 
+  // Toggles between break and session timers
   function toggleTimer() {
     const time = state.break ? state.breakLength * 60 : state.sessionLength * 60;
     const display = state.break ? `${state.breakLength}:00` : `${state.sessionLength}:00`;
@@ -99,6 +112,7 @@
     updateCounterDisplay(display);
   }
 
+  // Updates the position of the progress bar relative to the current count of the timer
   function updateProgressBar() {
     if (!state.break) {
       const progress = (state.currentCount / (state.sessionLength * 60)) * 100;
@@ -109,20 +123,18 @@
     }
   }
 
-  function stopTimer() {
-    clearInterval(timer);
-  }
-
+  // Counts down the timer and updates the view
   function countdown() {
-    const date = new Date(null, null);
+    const date = new Date(null, null); // Null values sets the hours and minutes to 0
     const decreaseCount = state.currentCount - 1;
     updateState({ currentCount: decreaseCount });
     date.setSeconds(state.currentCount);
-    const hms = toHMS(date)
+    const hms = toHMS(date);
     updateCounterDisplay(hms);
     updateProgressBar();
   }
 
+  // Returns the time in HH:MM:SS format
   function toHMS(time) {
     const hours = time.getHours() > 0 ? `${time.getHours()}:` : '';
     const minutes = time.getMinutes() > 0 ? `${time.getMinutes()}:` : '';
@@ -130,16 +142,19 @@
     return `${hours}${minutes}${seconds}`;
   }
 
+  // Helper function to update the state of the app
   function updateState(newState = {}) {
     state = Object.assign({}, state, newState);
   }
 
+  // Updates the display of the timer and session/break lengths
   function updateDisplay() {
     breakDisplay.textContent = state.breakLength;
     sessionDisplay.textContent = state.sessionLength;
     timerDisplay.textContent = state.sessionLength;
   }
 
+  // Updates the display of the timer
   function updateCounterDisplay(time) {
     timerDisplay.textContent = time;
   }
